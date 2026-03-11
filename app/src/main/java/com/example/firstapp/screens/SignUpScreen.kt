cd734081f5f63dc0c5510bda9f18ea5039a52897
Paul De La Toba <pauldelatoba9@gmail.com>
2026-03-11 08:28:03 -0700

    inputs validation

--- app/src/main/java/com/example/firstapp/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/firstapp/screens/SignUpScreen.kt
@@ -6,12 +6,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.firstapp.InsertButton
 import com.example.firstapp.InsertTextField
@@ -37,11 +49,89 @@ fun SignUpScreen(navController: NavController){
                 Color.Black
             )
 
-            InsertTextField("Name", false, modifier = Modifier.fillMaxWidth())
-            InsertTextField("Email", false, modifier = Modifier.fillMaxWidth())
-            InsertTextField("Phone Number", false, modifier = Modifier.fillMaxWidth())
-            InsertTextField("Password", true, modifier = Modifier.fillMaxWidth())
-            InsertTextField("Confirm Password", true, modifier = Modifier.fillMaxWidth())
+            var name by remember { mutableStateOf("") }
+            var email by remember { mutableStateOf("") }
+            var phoneNumber by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+            var confirmPassword by remember { mutableStateOf("") }
+
+            fun isValidName(name: String): Boolean {
+                return name.isNotBlank() && name.all { it.isLetter() }
+            }
+
+            fun isValidEmail(email: String): Boolean {
+                return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+            }
+
+            fun isValidPhoneNumber(phoneNumber: String): Boolean {
+                return phoneNumber.length == 10 && phoneNumber.all { it.isDigit() }
+            }
+
+            fun isValidPassword(password: String, confirmPassword: String): Boolean {
+                return password == confirmPassword
+            }
+
+            // Name text field
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                textStyle = TextStyle(fontSize = 18.sp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text
+                ), visualTransformation = VisualTransformation.None,
+                value = name,
+                onValueChange = {name = it},
+                label = { Text("Name") }
+            )
+
+            // Email text field
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                textStyle = TextStyle(fontSize = 18.sp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email
+                ), visualTransformation = VisualTransformation.None,
+                value = email,
+                onValueChange = {
+                    email = it
+                },
+                label = { Text("Email") }
+            )
+
+            // Phone number text field
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                textStyle = TextStyle(fontSize = 18.sp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Phone
+                ), visualTransformation = VisualTransformation.None,
+                value = phoneNumber,
+                onValueChange = {phoneNumber = it},
+                label = { Text("Phone Number") }
+            )
+
+            // Password text field
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                textStyle = TextStyle(fontSize = 18.sp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password
+                ), visualTransformation = PasswordVisualTransformation(),
+                value = password,
+                onValueChange = {password = it},
+                label = { Text("Password") }
+            )
+
+            // Confirm password text field
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                textStyle = TextStyle(fontSize = 18.sp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password
+                ), visualTransformation = PasswordVisualTransformation(),
+                value = confirmPassword,
+                onValueChange = {confirmPassword = it},
+                label = { Text("Confirm Password") }
+            )
 
             Row(
                 Modifier.fillMaxWidth(),
